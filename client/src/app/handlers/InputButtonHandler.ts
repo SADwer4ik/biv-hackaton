567d4a124ce75352bfb02f845bca300175993893
Юра <yurakorolev04@gmail.com>
2024-11-16 22:54:14 +0300

    Added dockerfile

---
+++ client/src/app/handlers/InputButtonHandler.ts
@@ -0,0 +1,17 @@
+export enum Responses {
+    BANK_SERVICE = "Банковские услуги",
+    FOOD_GOODS = "Продовольственные товары",
+    NON_FOOD_GOODS = "Непродовольственные товары",
+    LEASING = "Лизинг(финансовая аренда)",
+    LOAN = "Займы",
+    REALE_STATE = "Недвижимость",
+    SERVICE = "Услуги",
+    TAX = "Налоги",
+    NOT_CLASSIFIED = "Не подходит под предыдущие категории",
+}
+
+export default function InputButtonHandler(x: string): string {
+    console.log(x);
+    const t: string = "";
+    return t;
+}
